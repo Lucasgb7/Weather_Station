db1ca79eea7e9ab770e913239075bf7e080a249e
Lucas Cunha <lucasgb7_ks@hotmail.com>
2021-07-07 14:34:13 -0300

    teste temperatura/umidade

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,72 @@
-#include <Arduino.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+#define DHTPIN 4
+#define DHTTYPE    DHT11
+
+DHT_Unified dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS;
+
+void dht11_read(){
+  // Obtem um evento de temperatura e mostra o valor
+  sensors_event_t event;
+  dht.temperature().getEvent(&event);
+  if (isnan(event.temperature)) {
+    Serial.println(F("Error! Temperatura!?"));
+  }
+  else {
+    Serial.print(F("Temperatura: "));
+    Serial.print(event.temperature);
+    Serial.println(F("°C"));
+  }
+
+  // Obtem um evento de umidade e mostra o valor
+  dht.humidity().getEvent(&event);
+  if (isnan(event.relative_humidity)) {
+    Serial.println(F("Error! Umidade!?"));
+  }
+  else {
+    Serial.print(F("Umidade: "));
+    Serial.print(event.relative_humidity);
+    Serial.println(F("%"));
+  }
+}
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  // Inicializa DHT11
+  dht.begin();
+  Serial.println(F("DHTxx Unified Sensor Example"));
+  // Print temperature sensor details.
+  sensor_t sensor;
+  dht.temperature().getSensor(&sensor);
+  Serial.println(F("------------------------------------"));
+  Serial.println(F("Temperature Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("°C"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("°C"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("°C"));
+  Serial.println(F("------------------------------------"));
+  // Print humidity sensor details.
+  dht.humidity().getSensor(&sensor);
+  Serial.println(F("Humidity Sensor"));
+  Serial.print  (F("Sensor Type: ")); Serial.println(sensor.name);
+  Serial.print  (F("Driver Ver:  ")); Serial.println(sensor.version);
+  Serial.print  (F("Unique ID:   ")); Serial.println(sensor.sensor_id);
+  Serial.print  (F("Max Value:   ")); Serial.print(sensor.max_value); Serial.println(F("%"));
+  Serial.print  (F("Min Value:   ")); Serial.print(sensor.min_value); Serial.println(F("%"));
+  Serial.print  (F("Resolution:  ")); Serial.print(sensor.resolution); Serial.println(F("%"));
+  Serial.println(F("------------------------------------"));
+  // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Delay between measurements.
+  delay(delayMS);
+  dht11_read();
 }
