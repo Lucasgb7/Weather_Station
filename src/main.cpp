b416f33e0dc39e36fc4030a66542233a7cbac5f5
Lucas <lucasgb7_ks@hotmail.com>
2021-08-26 17:14:43 -0300

    chaning names, comments, libraries, and adding LoRaWAN settings

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,18 @@
 #include <Adafruit_BMP280.h>
 #include <Adafruit_Sensor.h>
 #include <ADSWeather.h>
+#include <ArduinoJson.h>
 #include <DHT.h>
-#include <DHT_U.h>
 #include <HardwareSerial.h>
 #include <SPI.h>
+#include "../lib/keys.h"
 #include "RoboCore_SMW_SX1276M0.h"
 #include <Wire.h>
 
 // Temperature/Humidity Sensor (DHT11)
 #define DHTPIN 4
-#define DHTTYPE    DHT11
-DHT_Unified dht(DHTPIN, DHTTYPE);
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
 
 // Temperature/Atmosferic Pressure Sensor (BMP280) 
 // using SPI interface
@@ -21,7 +22,7 @@ Adafruit_BMP280 bmp;
 byte I2C_ADRESS = 0x76; // '../test/getI2C.ino'.
 
 // UV Radiation Sensor (ML8511)
-#define ML8511PIN 15
+#define UV_PIN 15
 
 // Rain Sensor (PDB10U)
 #define RAIN_PIN 35
@@ -40,73 +41,53 @@ float mps, kph, windChill;
 int clicked, wspd, wdir, wdirRaw, wchill;
 ADSWeather ws1(RAIN_PIN, VANE_PIN, ANEMOMETER_PIN); //This should configure all pins correctly
 
-// LoRaWAN
-#if !defined(ARDUINO_ESP32_DEV) // ESP32
-#error Use this example with the ESP32
-#endif
-
-HardwareSerial LoRaSerial(2);
+// LoRaWAN settings
 #define RXD2 16
 #define TXD2 17
+#define ONBOARD_LED 2 // LoRaWAN Status LED
 
+HardwareSerial LoRaSerial(2);
 SMW_SX1276M0 lorawan(LoRaSerial);
-
 CommandResponse response;
 
-const char APPEUI[] = "0000000000000000";
-const char APPKEY[] = "00000000000000000000000000000000";
+// Keys are included by #include "../lib/keys.h"
+//const char APPEUI[] = "0000000000000000";
+//const char APPKEY[] = "00000000000000000000000000000000";
 
 const unsigned long PAUSE_TIME = 300000; // [ms] (5 min)
 unsigned long timeout;
 
-/*============================= FUNCTIONS =============================*/
+/*================================ FUNCTIONS ================================*/
+// Prototype
 void event_handler(Event);
 
-// Temperature/Humidity reading 
-void dht11_read(){
-  // Get a temperature event and its value
-  sensors_event_t event;
-  dht.temperature().getEvent(&event);
-  if (isnan(event.temperature)) {
-    Serial.println(F("Error! Temperature!?"));
-  }
-  else {
-    Serial.print(F("Temperatura: "));
-    Serial.print(event.temperature);
-    Serial.println(F(" °C"));
-  }
-
-  // Get a humidity event and its value
-  dht.humidity().getEvent(&event);
-  if (isnan(event.relative_humidity)) {
-    Serial.println(F("Error! Umidade!?"));
-  }
-  else {
-    Serial.print(F("Umidade do ar: "));
-    Serial.print(event.relative_humidity);
-    Serial.println(F(" %"));
-  }
+// DHT11 - Temperature
+float getTemperature()
+{
+  return dht.readTemperature();
 }
-// Atmosferic Pressure reading
-void bmp280_read(){
-    Serial.print(F("Temperatura: "));
-    Serial.print(bmp.readTemperature());
-    Serial.println(" °C");
 
-    Serial.print(F("Pressao Atmosferica: "));
-    Serial.print(bmp.readPressure()/100);
-    Serial.println(" hPa");
-
-    Serial.print(F("Altitude: "));
-    Serial.print(bmp.readAltitude(1019)); // pressao atmosferica no nivel do mar (variavel)
-    Serial.println(" m");
+// DHT11 - Humidity
+float getHumidity()
+{
+  return dht.readHumidity();
 }
+
+// BMP280 - Atmospheric Pressure
+float getPressure()
+{
+  // bmp.readTemperature();
+  // bmp.readAltitude(1019) 
+  return bmp.readPressure()/100; // return pressure in hPa
+}
+
 // Map for UV reading
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-// Returns average value from UV reading
+
+// Takes an average of readings on a given pin
 int averageAnalogRead(int pinToRead)
 {
   byte numberOfReadings = 8;
@@ -118,54 +99,41 @@ int averageAnalogRead(int pinToRead)
  
   return(runningValue);
 }
-// UV Radiation reading
-void ml8511_read(int SensorPIN){
+
+// ML8511 - UV Intensity
+float getUV(int SensorPIN)
+{
   int uvLevel = averageAnalogRead(SensorPIN);
- 
+
   float outputVoltage = 3.3 * uvLevel/4095;
   float uvIntensity = mapfloat(outputVoltage, 0.99, 2.9, 0.0, 15.0);
- 
-  Serial.print("Intensidade UV: ");
-  Serial.print(uvIntensity);
-  Serial.println(" mW/cm^2"); 
+
+  return uvIntensity;
 }
 
-void rain_read(int INPUTPIN){
-  val = digitalRead(INPUTPIN);      // Lê o Status do Reed Switch
-  if ((val == LOW) && (old_val == HIGH)) {   // Verefica se o Status mudou
-    delay(10);                   // Atraso colocado para lidar com qualquer "salto" no switch.
-    REEDCOUNT = REEDCOUNT + 1;   // Adiciona 1 à cntagem de pulsos
-    old_val = val;              //Iguala o valor antigo com o atual
- 
-    // Imprime no Monitor Serial
-    Serial.print("Medida de chuva (contagem): ");
-    Serial.print(REEDCOUNT);
-    Serial.println(" pulso");
-    Serial.print("Medida de chuva (calculado): ");
-    Serial.print(REEDCOUNT * 0.50);
-    Serial.println(" mm");
+// PDB10U - Rain Gauge
+float getRain(int SensorPIN)
+{
+  val = digitalRead(SensorPIN);
+  if ((val == LOW) && (old_val == HIGH)) {   
+    delay(10);              
+    REEDCOUNT = REEDCOUNT + 1;   
+    old_val = val;              
   }
+  if ((val == HIGH) && (old_val == LOW)) {   
+  delay(10);
+  REEDCOUNT = REEDCOUNT + 1;
+  old_val = val;
  
-    if ((val == HIGH) && (old_val == LOW)) {   // Verefica se o Status mudou
-    delay(10);                   // Atraso colocado para lidar com qualquer "salto" no switch.
-    REEDCOUNT = REEDCOUNT + 1;   // Adiciona 1 à cntagem de pulsos
-    old_val = val;              //Iguala o valor antigo com o atual
- 
-    // Imprime no Monitor Serial
-    Serial.print("Medida de chuva (contagem): ");
-    Serial.print(REEDCOUNT);
-    Serial.println(" pulso");
-    Serial.print("Medida de chuva (calculado): ");
-    Serial.print(REEDCOUNT * 0.50);
-    Serial.println(" mm");
-  }
- 
+  } 
   else {
-    old_val = val;              //If the status hasn't changed then do nothing
+    old_val = val;
   }
+  return REEDCOUNT * 0.5;
 }
 
-void read_windSpeed()
+// MISOL WH-SP-WS01 - Anemometer (Wind Speed Sensor)
+int getWindSpeed()
 {
     lastMillis = xTaskGetTickCount();
     while(xTaskGetTickCount() - lastMillis < 10000){
@@ -177,43 +145,16 @@ void read_windSpeed()
         if(digitalRead(ANEMOMETER_PIN) == LOW) state = false;
     }
 
-    mps = clicked * 0.0333; // metros/segundo
-    kph = mps * 3.6;        // kilometros/hora
+    mps = clicked * 0.0333; // m/s
+    kph = mps * 3.6;        // km/h
     wspd = int(mps*10);
     clicked = 0;
 
-    Serial.print("Velocidade do Vento: ");
-    Serial.print(wspd);
-    Serial.println(" m/s");
+    return wspd; // m/s
 }
 
-void read_windDirection()
-{
-  wdirRaw = analogRead(VANE_PIN);
-
-  if(wdirRaw > 2155 && wdirRaw < 2207) wdir = 0;
-  else if (wdirRaw > 502  && wdirRaw < 554) wdir = 22.5;
-  else if (wdirRaw > 649  && wdirRaw < 699) wdir = 45;
-  else if (wdirRaw > 49 && wdirRaw < 90) wdir = 112.5;
-  else if (wdirRaw > 89 && wdirRaw < 111) wdir = 135;
-  else if (wdirRaw > 0 && wdirRaw < 11) wdir = 157.5;
-  else if (wdirRaw > 242 && wdirRaw < 280) wdir = 180;
-  else if (wdirRaw > 159 && wdirRaw < 211) wdir = 202.5;
-  else if (wdirRaw > 1239 && wdirRaw < 1301) wdir = 225;
-  else if (wdirRaw > 1129 && wdirRaw < 1161) wdir = 247.5;
-  else if (wdirRaw > 4069 && wdirRaw < 5001) wdir = 270;
-  else if (wdirRaw > 2470 && wdirRaw < 2516) wdir = 292.5;
-  else if (wdirRaw > 3101 && wdirRaw < 3321) wdir = 315;
-  else if (wdirRaw > 1629 && wdirRaw < 1656) wdir = 337.5;
-  else if(wdirRaw == 0) wdir = 90;
-
-  Serial.print("wdirRaw: ");
-  Serial.println(wdirRaw);
-  Serial.print("Direção do Vento: ");
-  Serial.print(wdir);
-  Serial.println(" °");
-}
-void read_windDirection2() // V= 5V; R= 66KOhm;
+// MISOL WH-SP-WD - Wind Vane (Wind Directtion Sensor)
+int getWindDirection()
 {
   wdirRaw = analogRead(VANE_PIN);
 
@@ -233,30 +174,50 @@ void read_windDirection2() // V= 5V; R= 66KOhm;
   else if (wdirRaw > 2273 && wdirRaw <= 2917) wdir = 292.5; // 42k12- 292.5º- WNW
   else if (wdirRaw > 2917 && wdirRaw <= 3347) wdir = 315;   // 64k9 - 315º  - NW
   else if (wdirRaw > 1480 && wdirRaw <= 1916) wdir = 337.5; // 21k88- 337.5º- NNW
-  //else if(wdirRaw == 0) wdir = 90;
 
-  Serial.print("wdirRaw: ");
-  Serial.println(wdirRaw);
-  Serial.print("Direção do Vento: ");
-  Serial.print(wdir);
-  Serial.println(" °");
+  return wdir;
 }
 
+void blink(int LED_PIN)
+{
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+  digitalWrite(LED_PIN, HIGH);
+  delay(500);
+  digitalWrite(LED_PIN, LOW);
+  delay(500);
+}
 
+void sendData()
+{
+  DynamicJsonDocument jsonData(JSON_OBJECT_SIZE(7));
+  jsonData["T"] = getTemperature();
+  jsonData["H"] = getHumidity();
+  jsonData["P"] = getPressure();
+  jsonData["U"] = getUV(UV_PIN);
+  jsonData["D"] = getWindDirection();
+  jsonData["S"] = getWindSpeed();
+  jsonData["R"] = getRain(RAIN_PIN);
+
+  String payload = "";
+  serializeJson(jsonData, payload);
+  Serial.print("Data sent: ");
+  Serial.println(payload);
+
+  lorawan.sendT(1, payload);
+  blink(ONBOARD_LED); // reporting a sent status on LED
+}
 
 void setup() {
   Serial.begin(115200);
-
+  pinMode(ONBOARD_LED, OUTPUT);
   /*=============================== SENSORS ===============================*/
   // Temperature and Humidty Sensor DHT11
   dht.begin();
-  sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
-  dht.humidity().getSensor(&sensor);
   // Pressure Sensor BMP280
   bmp.begin(I2C_ADRESS);
   // UV Sensor
-  pinMode(ML8511PIN, INPUT);
+  pinMode(UV_PIN, INPUT);
   // Rain gauge sensor
   pinMode (RAIN_PIN, INPUT_PULLUP); //This activates the internal pull up resistor
   // Wind wane sensor
@@ -271,7 +232,7 @@ void setup() {
   lorawan.event_listener = &event_handler;
   Serial.println(F("Handler set"));
 
-  // read the Device EUI
+  // Lemos o DEVEUI do modulo LoRaWAN Bee e mostramos no Monitor Serial
   char deveui[16];
   response = lorawan.get_DevEUI(deveui);
   if(response == CommandResponse::OK){
@@ -281,29 +242,70 @@ void setup() {
   } else {
     Serial.println(F("Error getting the Device EUI"));
   }
+
+  // Configuramos a chave APPEUI no modulo e mostramos no Monitor Serial
+  response = lorawan.set_AppEUI(APPEUI);
+  if(response == CommandResponse::OK){
+    Serial.print(F("Application EUI set ("));
+    Serial.write((uint8_t *)APPEUI, 16);
+    Serial.println(')');
+  } else {
+    Serial.println(F("Error setting the Application EUI"));
+  }
+
+  // Configuramos a chave APPKEY no modulo e mostramos no Monitor Serial
+  response = lorawan.set_AppKey(APPKEY);
+  if(response == CommandResponse::OK){
+    Serial.print(F("Application Key set ("));
+    Serial.write((uint8_t *)APPKEY, 32);
+    Serial.println(')');
+  } else {
+    Serial.println(F("Error setting the Application Key"));
+  }
+
+  // Configuramos o modo de operacao do LoRaWAN Bee para OTAA
+  response = lorawan.set_JoinMode(SMW_SX1276M0_JOIN_MODE_OTAA);
+  if(response == CommandResponse::OK){
+    Serial.println(F("Mode set to OTAA"));
+  } else {
+    Serial.println(F("Error setting the join mode"));
+  }
+
+  // Comecamos as tentativas para conexao na rede LoRaWAN da ATC
+  Serial.println(F("Joining the network"));
+  lorawan.join();
+
 }
 
-void loop() {
-  Serial.println("================================");
-  Serial.println("----- DHT11 -----");
-  dht11_read();
-  Serial.println("----- BMP280 -----");
-  bmp280_read();
-  Serial.println("----- ML8511 -----");
-  ml8511_read(ML8511PIN);
-  Serial.println("----- PLUVIOMETRO -----");
-  rain_read(RAIN_PIN);
-  Serial.println("----- ANEMOMETRO -----");
-  read_windSpeed();
-  Serial.println("----- CATA-VENTO -----");
-  read_windDirection2();
-  Serial.println("================================");
-  delay(2000);
+void loop() 
+{
+  // "Escutamos" se algo vem do modulo LoRaWAN Bee
+  lorawan.listen();
+
+  // Se esta conectado a rede entra nesta rotina
+  if(lorawan.isConnected()){
+    
+   	// A cada PAUSE_TIME milisegundos, acessamos a funcao de envio de dados
+    if(timeout < millis()){
+
+      sendData();
+      
+      timeout = millis() + PAUSE_TIME;
+    }
+  } else {
+    // Se nao conseguir se conectar a rede LoRaWAN, imprime no 
+    // Monitor Serial "." a cada 5 segundos
+    if(timeout < millis()){
+      Serial.println('...');
+      timeout = millis() + 5000; // 5 s
+    }
+  }
 }
 
 void event_handler(Event type){
   // check if join event
   if(type == Event::JOINED){
     Serial.println(F("Joined"));
+    digitalWrite(ONBOARD_LED, HIGH);
   }
 }
