10dbccc1d73be93a3394144a5a40f6b9268b80de
Lucas <lucasgb7_ks@hotmail.com>
2021-11-11 08:54:20 -0300

    data validation

--- data/readJSON.py
+++ data/readJSON.py
@@ -1,7 +1,8 @@
 import json
-from datetime import datetime 
+from datetime import datetime
+import xlsxwriter
 
-fileDir = 'data\WeatherStationData_09-11.json'
+fileDir = 'download_weather-station.json'
 
 with open(fileDir, 'r') as data_file:
     data = json.load(data_file)
@@ -11,19 +12,35 @@ for element in list(data):
     if element != 'variables':
         data.pop(element, None)
 
+workbook = xlsxwriter.Workbook('Dados_Estacao_LoRaWAN.xlsx')
+worksheet = workbook.add_worksheet()
 
-with open('data\data.txt', 'w') as data_file:
+worksheet.write(1, 0, "Data/Hora")
+col = 1
+
+with open('data.txt', 'w') as data_file:
     for variable in list(data['variables']):
-        print('\n')
-        print('Variavel: ', variable['variable_name'])
-        data_file.write("\n\nVariavel: ")
-        data_file.write(variable['variable_name'])
-        data_file.write("\n")
-
+        #print('\n')
+        #print('Variavel: ', variable['variable_name'])
+        #data_file.write("\n\nVariavel: ")
+        #data_file.write(variable['variable_name'])
+        #data_file.write("\n")
+        row = 1
+        worksheet.write(row, col, variable['variable_name'])
+        row += 1
+        col += 1
+        
         for v in variable['histories']:
-            print('Value:', v['value'])
-            v['timestamp'] = datetime.fromtimestamp(v['timestamp']/1000)
-            print('Timestamp:', v['timestamp'])
+            #print('Value:', v['value'])
+            v['timestamp'] = datetime.utcfromtimestamp(v['timestamp']/1000).strftime('%Y-%m-%d %H:%M')
+            #print('Timestamp:', v['timestamp'])
 
-            data_file.write("Valor: "); data_file.write(v['value']); data_file.write("\n")
-            data_file.write("Timestamp: "); data_file.write(str(v['timestamp'])); data_file.write("\n")
+            #data_file.write("Valor: "); data_file.write(v['value']); data_file.write("\n")
+            #data_file.write("Timestamp: "); data_file.write(str(v['timestamp'])); data_file.write("\n")
+            worksheet.write(row, 0, str(v['timestamp']))
+            worksheet.write(row, col-1, v['value'])
+            row += 1
+
+
+print("Done")
+workbook.close()
